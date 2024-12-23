6ba2bf71d9a0b0ec5dcc295097f662cd21d1bdd9
Saint <188669077+Sanctana@users.noreply.github.com>
2024-12-23 10:23:29 +0800

    Create BAPe04.c

---
+++ Solutions/C/02C Operators/C Arithmetic Operators/Basic_Arithmetic-ProblemsC/EasyC/BAPe04.c
@@ -0,0 +1,22 @@
+// December 23, 2024 - with pseudo
+// Problem: Multiply 4 by the sum of 6 and 3.
+// Expected Output: 36
+#include <stdio.h>
+
+int main(){// START
+// SET a = 4, b = 6, c = 3
+    int a = 4;
+    int b = 6;
+    int c = 3;
+
+// DECLARE result
+    int result;
+    
+// SET result and calculate, add 6 and 3 first, then multiply by 4
+// (b+c)*a
+    result = (b+c)*a;
+
+// OUTPUT result
+    printf("The result is: %d", result);
+    
+}// END 
