8d8bf13cfaff609d0f438b20cd98f6443bbe5fe9
Saint <188669077+Sanctana@users.noreply.github.com>
2024-12-23 10:04:52 +0800

    Create BAPe01.c

---
+++ Solutions/C/02C Operators/C Arithmetic Operators/Basic_Arithmetic-ProblemsC/EasyC/BAPe01.c
@@ -0,0 +1,21 @@
+// December 23, 2024 - with pseudo
+// Problem: add 12 and 8, then subtract 5.
+#include <stdio.h>
+
+int main(){// START
+// SET a = 12, b = 8, c = 5
+    int a = 12;
+    int b = 8;
+    int c = 5;
+    
+// DECLARE result
+    int result;
+    
+// SET result = (a + b) - c, Calculate result
+    result = (a+b)-c;
+    
+// OUTPUT result, "The result is: "result
+    printf("The result is: %d", result);
+    return 0;
+    
+}// END
