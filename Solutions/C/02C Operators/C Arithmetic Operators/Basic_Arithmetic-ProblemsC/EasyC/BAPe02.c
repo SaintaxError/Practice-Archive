cc1fdaae44c3e51612c6e3edb124174dc2c8e405
Saint <188669077+Sanctana@users.noreply.github.com>
2024-12-23 10:11:56 +0800

    Create BAPe02.c

---
+++ Solutions/C/02C Operators/C Arithmetic Operators/Basic_Arithmetic-ProblemsC/EasyC/BAPe02.c
@@ -0,0 +1,17 @@
+// December 23, 2024 - with pseudo
+// Problem: Subtract 7 from 20, then multiply the result by 3.
+// Expected Output: 39
+#include <stdio.h>
+
+int main(){// START
+// SET a = 7, b = 20, c = 3
+    int a = 7;
+    int b = 20;
+    int c = 3;
+// DECLARE result
+    int result;
+// SET result and calculate, (20-7)*3
+    result = (b-a)*c;
+// OUTPUT result
+    printf("The result is: %d", result);
+}// END
