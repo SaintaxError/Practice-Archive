37cdb5fe231ecede4f5d7b0201617eae11947a73
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-26 03:35:42 +0800

    Delete Solutions/JScript/01JS Input and Output/Output_Operations-ProblemsJS directory

--- Solutions/JScript/01JS Input and Output/Output_Operations-ProblemsJS/OOP01.js
+++
@@ -1,8 +0,0 @@
-// December 26, 2024 
-/* Problem: Create a program that prints a simple sentence, "Welcome to the world of programming!"
-
-Expected Output:
-Welcome to the world of programming!
-*/
-
-console.log("Welcome to the world of programming!");
