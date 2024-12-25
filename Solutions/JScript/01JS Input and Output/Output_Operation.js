c4a1187bf27f302ed11724f52fccebcfeec57a15
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-26 03:22:12 +0800

    Create Output_Operation.js

---
+++ Solutions/JScript/01JS Input and Output/Output_Operation.js
@@ -0,0 +1,22 @@
+
+/* Problem: Print a pattern of numbers from 1 to 5 in a simple vertical list. Each number should be on a new line.
+
+Expected Output:
+1
+2
+3
+4
+5
+
+*/
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+/* i can use newline but gi tagsa lang jud nako for practice,
+also compared to c lahi ni siyaa, sa c diba kada printf nimo kay if
+dili nimo butangan ug /n kay mo sikit ra siya, but sa js, lahi,
+kada output nimo every newline ana nga nag output ka naa siyay 
+newline */
