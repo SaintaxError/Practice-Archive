bb2d063b8800178af35314fd1ff55c70db8b7dab
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-26 02:33:52 +0800

    Update and rename Solutions/JScript/01JS Output Operations to Solutions/JScript/01JS Input and Output/Output_Operations-ProblemsJS/OOP01.js

---
+++ Solutions/JScript/01JS Input and Output/Output_Operations-ProblemsJS/OOP01.js
@@ -0,0 +1,8 @@
+// December 26, 2024 
+/* Problem: Create a program that prints a simple sentence, "Welcome to the world of programming!"
+
+Expected Output:
+Welcome to the world of programming!
+*/
+
+console.log("Welcome to the world of programming!");
