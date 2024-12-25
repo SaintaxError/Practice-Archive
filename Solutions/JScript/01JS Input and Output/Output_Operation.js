45fcd0215b968fb1f34f50021808a1fd1318ea96
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-26 03:35:25 +0800

    Update Output_Operation.js

--- Solutions/JScript/01JS Input and Output/Output_Operation.js
+++ Solutions/JScript/01JS Input and Output/Output_Operation.js
@@ -1,3 +1,15 @@
+// December 26, 2024
+
+/* Problem: Create a program that prints a simple sentence, "Welcome to the world of programming!"
+
+Expected Output:
+Welcome to the world of programming!
+*/
+
+console.log("1. Simple Sentence\n");
+// code
+console.log("Welcome to the world of programming!");
+
 
 /* Problem: Print a pattern of numbers from 1 to 5 in a simple vertical list. Each number should be on a new line.
 
@@ -10,6 +22,8 @@ Expected Output:
 
 */
 
+console.log("\n2. Patterns of Number\n");
+// code
 console.log("1");
 console.log("2");
 console.log("3");
@@ -20,3 +34,58 @@ also compared to c lahi ni siyaa, sa c diba kada printf nimo kay if
 dili nimo butangan ug /n kay mo sikit ra siya, but sa js, lahi,
 kada output nimo every newline ana nga nag output ka naa siyay 
 newline */
+
+
+/* Problem: Print the list of three movies, each on a new line.
+
+Expected Output:
+The Matrix
+Inception
+The Dark Knight
+
+*/
+console.log("\n3. List of Movies\n");
+// code
+console.log("The Matrix");
+console.log("Inception");
+console.log("The Dark Knight");
+
+
+/* Problem: Display a user profile with the following details: Name, Age, Location, Occupation. The output should follow the format shown below.
+
+Expected Output:
+Name: John Doe
+Age: 30
+Location: New York
+Occupation: Developer
+
+*/
+
+console.log("\n4. User Profile\n");
+// code
+console.log("Name: John Doe");
+console.log("Age: 30");
+console.log("Location: New York");
+console.log("Occupation: Developer");
+
+
+/* Problem: Print a simple daily schedule for a student, listing the time and activity.
+
+Expected Output:
+7:00 AM - Wake up
+8:00 AM - Study
+12:00 PM - Lunch
+1:00 PM - Study
+5:00 PM - Exercise
+7:00 PM - Dinner
+
+*/
+
+console.log("\n5. Daily Schedule\n");
+// code
+console.log("7:00 AM - Wake up");
+console.log("8:00 AM - Study");
+console.log("12:00 PM - Lunch");
+console.log("1:00 AM - Study");
+console.log("5:00 PM - Exercise");
+console.log("7:00 PM - Dinner");
