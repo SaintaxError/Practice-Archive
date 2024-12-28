e75a31b90c66ce8a8e7285f2ff082bf019491769
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-28 22:47:07 +0800

    Create Medium.js

---
+++ Solutions/JScript/02JS Operators/JS Arithmetic Operators/ Basic_Arithmetic-ProblemsJS/Medium.js
@@ -0,0 +1,33 @@
+// December 28, 2024
+
+1. Add, Multiply, and Subtract
+
+function result1(x, y){
+  return (((x + y) * 3) - 10);
+}
+// INPUT and OUTPUT x,y
+  let x = prompt("Enter x: "),
+      y = prompt("Enter y: ");
+
+x = Number(x);
+y = Number(y);
+
+console.log(`Result: ${result1(x,y)}`);
+
+
+2. Multiply, Divide, and Add
+
+function result2(x,y){
+  return ((x * y) / 4) + 5;
+}
+
+let x = prompt("Enter x: "),
+    y = prompt("Enter y: ");
+
+x = Number(x);
+y = Number(y);
+
+console.log(`Result: ${result2(x,y)}`);
+
+
+// kapoy tiwas zZz
