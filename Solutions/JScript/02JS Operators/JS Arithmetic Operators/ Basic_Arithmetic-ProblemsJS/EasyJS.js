807ebae9d1ed3be137a654d9282ebbdad006b4fb
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2024-12-28 22:34:01 +0800

    Update EasyJS.js

--- Solutions/JScript/02JS Operators/JS Arithmetic Operators/ Basic_Arithmetic-ProblemsJS/EasyJS.js
+++ Solutions/JScript/02JS Operators/JS Arithmetic Operators/ Basic_Arithmetic-ProblemsJS/EasyJS.js
@@ -1,1 +1,71 @@
+// December 28, 2024 - learning using function in JS nalang, para mapractice daan
+
+1. Add and Subtract
+
+function result1(){
+    // DECLARE a = 30, b = 5, c = 12
+    let a = 12, b = 8, c = 5;
+    return (a + b) - c; // Calculate
+}
+// OUTPUT
+  console.log(result1());
+
+
+
+
+2. Subtraction and Multiplication
+
+function result2(){
+  // DECLARE a = 30, b = 5, c = 12
+  let a = 7, b = 20, c = 3;
+  return (b - a) * c; // Calculate
+}
+// OUTPUT
+  console.log(result2());
+
+
+
+
+3. Division and Addition 
+
+function result3(){
+  // DECLARE a = 30, b = 5, c = 12
+  let a = 30, b = 5, c = 12;
+  return (a / b) + c; // Calculate
+}
+// OUTPUT
+  console.log(result3());
+
+
+
+
+
+4. Multiplication and Sum
+  
+function result4(){
+  //DECLARE a = 4, b = 6, c = 3
+  let a = 4, b = 6, c = 3;
+  return a * (b + c); // Calculate
+}
+//OUTPUT
+  console.log(result4());
+
+
+
+
+
+5. Remainder and Addition
+
+function result5(){
+    // DECLARE a = 27, b = 4, c = 6
+    let a = 27, b = 4, c = 6;
+    return (a % b) + c; // Calculate
+}
+//OUTPUT
+  console.log(result5());
+
+
+
+  
+  
 
