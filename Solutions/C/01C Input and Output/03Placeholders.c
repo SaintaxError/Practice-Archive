19043e9b411390ffc46728f5fc1a359f564e01bc
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2025-01-02 21:51:40 +0800

    Create 03Placeholders.c

---
+++ Solutions/C/01C Input and Output/03Placeholders.c
@@ -0,0 +1,22 @@
+1. Conference Speaker Announcement
+
+#include <stdio.h>
+
+int main(){
+    char speaker[20] = "Dr.Emily Rose", session[50] = "The Future of Artificial Intelligence", datesession[20] = "August 10,2025",
+    timepm[20] = "2:00 PM";
+    
+    printf("Speaker Name: %s\n", speaker);
+    printf("Session Title: %s\n", session);
+    printf("Date: %s\n", datesession);
+    printf("Time: %s\n\n", timepm);
+    printf("Join us in the main hall for an inspiring talk!");
+    
+    return 0;
+}
+
+2. Restaurant Receipt
+
+3. Sports Tournament Registration
+4. Movie Reservation Details
+5. Online Course Enrollment Details
