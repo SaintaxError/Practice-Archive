aa49bcc302cc38297b9c94ffe85d46e42d247321
Saint 一切顺利 <188669077+Sanctana@users.noreply.github.com>
2025-01-02 21:28:52 +0800

    Create 02Basic_Escape-Sequence.js

---
+++ Solutions/C/01C Input and Output/02Basic_Escape-Sequence.js
@@ -0,0 +1,87 @@
+// January 2, 2025
+
+1. Item Price Discount
+
+#include <stdio.h>
+
+int main(){
+    printf("Item: Shoes\n");
+    printf("\tPrice: $50\n");
+    printf("\tDiscount: $10");
+    return 0;
+}
+
+
+
+
+
+2. Backlash & Quotes
+
+#include <stdio.h>
+
+int main(){
+    printf("To include a backslash, use \\ and to include quotes, use \" around the text.");
+    return 0;
+}
+
+
+
+
+
+3. Shopping List with Formatting
+
+#include <stdio.h>
+
+int main(){
+    printf("-----------------------------\n");
+    printf("Shopping List\n");
+    printf("-----------------------------\n");
+    printf("Item\t\tPrice\n");
+    printf("-----------------------------\n");
+    printf("Apples\t\t$2.50\n");
+    printf("Bananas\t\t$1.30\n");
+    printf("Oranges\t\t$3.00\n");
+    printf("Grapes\t\t$4.75\n");
+    printf("-----------------------------\n");
+    printf("Total Price: $11.55\n");
+    printf("-----------------------------");
+    
+    return 0;
+}
+
+
+
+
+
+4. File Path Display
+
+#include <stdio.h>
+
+int main(){
+    printf("File Path: C:\\Users\\JohnDoe\\Documents\\\"My Files\"\\Report.txt\n");
+    printf("Size: 1.5 MB\n");
+    printf("Last Modified: 12/22/2024\n");
+    printf("---------------------------");
+    
+    return 0;
+}
+
+
+
+
+
+5. Movie Review
+
+#include <stdio.h>
+
+int main(){
+    printf("Moview Review\n");
+    printf("--------------------------\n");
+    printf("Title: \"Inception\"\n");
+    printf("Director: Christopher Nolan\n");
+    printf("Rating: 9.5/10\n");
+    printf("Review: \"A mind-bending thriller that explores the concept of dreams within dreams. A must-watch for sci-fi lovers!\n");
+    printf("--------------------------");
+  
+    return 0;
+}
